d489b9a52afa8b6b6216d7458b7f295d10d152d2
jmoreno15 <jdmoreno159@gmail.com>
2026-09-11 16:24:14 -0500

    Cambia envio de correo a puerto 465 SSL para compatibilidad con Railway

--- src/main/java/com/johannmoreno/vueloapp/infrastructure/email/EmailService.java
+++ src/main/java/com/johannmoreno/vueloapp/infrastructure/email/EmailService.java
@@ -23,9 +23,12 @@ public class EmailService {
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.connectiontimeout", "10000");
+        props.put("mail.smtp.timeout", "10000");
 
         Session session = Session.getInstance(props, new jakarta.mail.Authenticator() {
             @Override
